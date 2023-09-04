1178670c599598124d52d918bcab5b3eb37bfaaf
Neo <hwhsong@163.com>
2023-09-04 11:08:42 +0800

    Flask is a lightweight WSGI web application framework

--- flask/start.py
+++ flask/start.py
@@ -25,15 +25,23 @@ CORS(app)
 
 # 通过装饰器对路由和函数进行映射
 @app.route('/', methods=['GET'])
+@app.route('/home/', methods=['GET'])
 def home():
     user = request.args.get('user') or 'world'  # 获取请求参数
     return '<h1>Hello, {0}</h1>'.format(user)
 
 
+# 在路由中设置参数
+@app.route('/hello/', methods=['GET'])
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name='world'):
+    return '<h1>Hello, {0}</h1>'.format(name)
+
+
 if __name__ == '__main__':
     # 启动应用
     app.run(
-        host='localhost',
-        port='8080',
-        debug=True
+        host='127.0.0.1',  # 设置为0.0.0.0以使服务器在外部可用
+        port=5000,  # 默认值
+        debug=True  # 开启调试模式
     )
